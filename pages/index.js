947a9eed47c15a13f69624f677ea627501fdcc35
Vance <thevancetan@gmail.com>
2020-11-26 15:35:17 +0800

    Updated data fetching for items

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useQuery, usePaginatedQuery } from "react-query";
+import { useRouter } from "next/router";
 
 import Image from "../public/hero-image.svg";
+import SearchSVG from "../public/search.svg";
+import StarSVG from "../public/star.svg";
+import FilterSVG from "../public/filter.svg";
+import PlusSVG from "../public/plus.svg";
+import ChevronLeftSVG from "../public/chevron-left.svg";
 
 import Stack from "../components/Stack";
-import TableItem from "../components/TableItem";
 
 const Container = styled.main`
   padding-left: calc(var(--rhythm) * 2);
@@ -51,7 +58,7 @@ const Content = styled.section`
   flex-direction: column;
   position: relative;
   z-index: 1;
-  height: 200vh;
+  /* height: 200vh; */
   background-color: var(--color-element);
   border-radius: 12px;
   box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
@@ -60,78 +67,167 @@ const Content = styled.section`
 `;
 
 const Toolbar = styled.div`
-  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  height: 70px;
   border-bottom: 3px solid var(--color-grey);
+  padding: 0 40px;
+
+  & > * + * {
+    margin-left: 15px;
+  }
+`;
+
+const Search = styled.input`
+  font-family: var(--font-primary);
+  font-size: var(--s0);
+  width: 100%;
+  border-radius: 12px 12px 0 0;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: #9aafcb;
+  }
+`;
+
+const Pagi = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: var(--font-primary);
+  font-size: var(--s1);
+  color: #9aafcb;
+  width: 100px;
+  transform: translateY(1px);
+
+  & > * + * {
+    margin-left: 5px;
+  }
+`;
+
+const SearchIcon = styled(SearchSVG)`
+  width: 40px;
+  stroke: #9aafcb;
+  stroke-width: 2px;
+  fill: none;
+`;
+
+const FilterIcon = styled(FilterSVG)`
+  width: 35px;
+  stroke: #9aafcb;
+  stroke-width: 2px;
+  fill: none;
+  transform: translateY(2px);
+`;
+
+const PlusIcon = styled(PlusSVG)`
+  width: 40px;
+  stroke: #9aafcb;
+  stroke-width: 2px;
+  fill: none;
+  transform: translateY(2px);
+`;
+
+const ChevronLeftIcon = styled(ChevronLeftSVG)`
+  width: 28px;
+  stroke: #9aafcb;
+  stroke-width: 2px;
+  fill: none;
+  transform: translateY(2px);
+`;
+
+const ChevronRightIcon = styled(ChevronLeftIcon)`
+  transform: rotate(180deg) translateY(-2px);
 `;
 
 const Table = styled.div`
-  display: flex;
-  flex-direction: column;
+  padding: 50px 20px;
+  --auto-grid-min-size: 250px;
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(var(--auto-grid-min-size), 1fr)
+  );
+  grid-gap: 20px;
 `;
 
-const books = [
-  {
-    title:
-      "Interplanetary Huntress: A Science Fiction Classic (The Adventures of Gerry Carlyle Book 1)",
-    author: "Arthur K. Barnes",
-    genre: "Science Fiction & Fantasy",
-    rating: "4.31",
-    numReviews: "1322",
-  },
-  {
-    title:
-      "Pilot in Command: Strategic Action Plan for Reducing Pilot Error (Practical Flying)",
-    author: "Paul A. Craig",
-    genre: "Engineering & Transportation",
-    rating: "4.07",
-    numReviews: "18923",
-  },
-  {
-    title:
-      "The McKinsey Way: Using the Techniques of the World's Top Strategic Consultants to Help You and Your Business",
-    author: "Ethan M. Rasiel",
-    genre: "Business & Money",
-    rating: "3.78",
-    numReviews: "456",
-  },
-  {
-    title:
-      "Interplanetary Huntress: A Science Fiction Classic (The Adventures of Gerry Carlyle Book 1)",
-    author: "Arthur K. Barnes",
-    genre: "Science Fiction & Fantasy",
-    rating: "4.31",
-    numReviews: "37",
-  },
-  {
-    title:
-      "Pilot in Command: Strategic Action Plan for Reducing Pilot Error (Practical Flying)",
-    author: "Paul A. Craig",
-    genre: "Engineering & Transportation",
-    rating: "4.07",
-    numReviews: "94",
-  },
-  {
-    title:
-      "The McKinsey Way: Using the Techniques of the World's Top Strategic Consultants to Help You and Your Business",
-    author: "Ethan M. Rasiel",
-    genre: "Business & Money",
-    rating: "3.78",
-    numReviews: "389",
-  },
-];
+const GridItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 250px;
+  /* border: 1px solid red; */
+`;
 
-const books2 = [
-  {
-    title: "Gold Coffin Murder Case",
-    author: "George F. Worts",
-    img:
-      "http://ecx.images-amazon.com/images/I/2136NBNV5FL._BO2,204,203,200_PIsitb-sticker-v3-xsmall,TopRight,2,-18_SX278_SY278_PIkin4,BottomRight,1,22_AA300_SH20_OU01_.jpg",
-    genre: "Mystery & Suspense",
-    reviews: "4.31",
-  },
-];
+const ItemImg = styled.img`
+  height: 85%;
+  object-fit: contain;
+  /* border: 1px solid blue; */
+`;
+
+const ItemStars = styled.div`
+  display: flex;
+  justify-content: center;
+  font-family: var(--font-primary);
+  font-size: var(--s-1);
+  color: var(--color-text);
+  /* border: 1px solid green; */
+`;
+
+const StarIcon = styled(StarSVG)`
+  width: 20px;
+  margin-left: 5px;
+  fill: #f6e05e;
+`;
+
+function useURLQuery() {
+  const router = useRouter();
+  return new URLSearchParams(router.asPath.slice(router.pathname.length));
+}
+
+function fetchItems(page) {
+  return new Promise(async (resolve) => {
+    const fetched = await fetch(
+      `http://localhost:3001/api/items?page=${page}`
+    ).then((res) => res.json());
+
+    resolve(fetched.data);
+  });
+}
+
+function fetchCount() {
+  return new Promise(async (resolve) => {
+    const fetched = await fetch(`http://localhost:3001/api/count`).then((res) =>
+      res.json()
+    );
+
+    resolve(fetched.data);
+  });
+}
 
 export default function Home() {
+  const pageQuery = parseInt(useURLQuery().get("page"), 10);
+  const [page, setPage] = useState(pageQuery ? pageQuery : 1);
+
+  const {
+    isLoading: countLoading,
+    isError: countError,
+    data: count,
+  } = useQuery("todos", fetchCount);
+  const {
+    isLoading: itemsLoading,
+    isError: itemsError,
+    resolvedData: items,
+  } = usePaginatedQuery(["items", page], () => fetchItems(page));
+
+  useEffect(() => {
+    history.pushState({}, "", `/?page=${page}`);
+  }, [page]);
+
   return (
     <Container>
       <Hero>
@@ -148,11 +244,40 @@ export default function Home() {
         <HeroImage />
       </Hero>
       <Content>
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <SearchIcon />
+          <Search placeholder="Search by title or author" />
+          <Pagi>
+            <button onClick={() => setPage(Math.max(1, page - 1))}>
+              <ChevronLeftIcon />
+            </button>
+            <span>{page}</span>
+            <button onClick={() => setPage(page + 1)}>
+              <ChevronRightIcon />
+            </button>
+          </Pagi>
+          <button>
+            <FilterIcon />
+          </button>
+          <button>
+            <PlusIcon />
+          </button>
+        </Toolbar>
         <Table>
-          {books.map((book, idx) => (
-            <TableItem even={idx % 2 === 0} {...book} />
-          ))}
+          {itemsLoading ? (
+            <div>Loading...</div>
+          ) : itemsError ? (
+            <div>Error: {error.message}</div>
+          ) : (
+            items.map((book, idx) => (
+              <GridItem>
+                <ItemImg src={book.imUrl} />
+                <ItemStars>
+                  {5} <StarIcon /> (234 Reviews)
+                </ItemStars>
+              </GridItem>
+            ))
+          )}
         </Table>
       </Content>
     </Container>
